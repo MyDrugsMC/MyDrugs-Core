1ea03963647219c27a1dd914768a9045d7b38028
Deo-Favente <enzo.dubois@etu.univ-grenoble-alpes.fr>
2026-03-16 12:39:10 +0100

    Initial first increment

---
+++ src/main/java/org/mydrugs/mydrugs-core/DrugEffect.java
@@ -0,0 +1,13 @@
+package org.mydrugs.mydrugs.core;
+
+public class DrugEffect {
+    private final Effect effect;
+
+    public DrugEffect(Effect effect) {
+        this.effect = effect;
+    }
+
+    public Effect getEffect() {
+        return effect;
+    }
+}
---
+++ src/main/java/org/mydrugs/mydrugs-core/EffectType.java
@@ -0,0 +1,7 @@
+package org.mydrugs.mydrugs.core;
+
+public enum EffectType {
+    INGAME_EFFECT,
+    SHADER,
+    INGAME_PERMANENT_BUFF;
+}
